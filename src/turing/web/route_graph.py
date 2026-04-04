70b856679d858d39396e97b1c0a0ceb224a39715
Christian Braun <braun.christian.1994@gmail.com>
2026-04-04 23:59:33 +0200

    init

--- src/turing/web/route_graph.py
+++ src/turing/web/route_graph.py
@@ -3,6 +3,19 @@ from bokeh.models import Plot
 import sys
 import os
 
+from threading import Thread
+
+from flask import Flask, render_template
+from tornado.ioloop import IOLoop
+
+from bokeh.embed import server_document
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.plotting import figure
+from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
+from bokeh.server.server import Server
+from bokeh.themes import Theme
+
 SCRIPT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(SCRIPT_DIR)
 
@@ -33,7 +46,7 @@ def prepare_inline() -> tuple[str, str]:
     return js_resources, css_resources
 
 
-@bp_p_graph.route('/graph')
+@bp_p_graph.route('/graph2')
 def prepare_template_graph():
     from flask import render_template
     import route_graph_util as wb
@@ -54,3 +67,32 @@ def prepare_template_graph():
     )
 
     return html
+
+
+def bkapp(doc):
+    import route_graph_util as wb
+    doc.theme = 'carbon'
+    G = wb.get_net_data()
+    G = r.graph.graph_to_nx(r.mermaid_td.read_mermaid_flowchart(r.mermaid_td.mermaid.splitlines()))
+    p = wb.get_netgraph(G=G, plot_title='state2 diagram')
+
+    doc.add_root(p)
+
+    doc.theme = 'carbon'
+
+
+@bp_p_graph.route('/graph', methods=['GET'])
+def bkapp_page():
+    script = server_document('http://localhost:5006/bkapp')
+    return render_template("__base_graph.html", div=script, template="Flask")
+
+
+def bk_worker():
+    # Can't pass num_procs > 1 in this configuration. If you need to run multiple
+    # processes, see e.g. flask_gunicorn_embed.py
+    server = Server({'/bkapp': bkapp}, io_loop=IOLoop(), allow_websocket_origin=["localhost:8000"])
+    server.start()
+    server.io_loop.start()
+
+
+Thread(target=bk_worker).start()
