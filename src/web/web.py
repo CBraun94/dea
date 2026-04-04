cf746e94cb0f6cd0cb9e1fb058e7dfb630a7140d
Christian Braun <braun.christian.1994@gmail.com>
2026-04-04 09:54:07 +0200

    init

--- src/web/web.py
+++ src/web/web.py
@@ -5,38 +5,9 @@ from bokeh.plotting import figure
 from bokeh.resources import INLINE
 
 
+template_html = 'embed.html'
+
 app = Flask(__name__)
-
-
-
-@app.route('/test')
-def bokeh():
-
-    # init a basic bar chart:
-    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
-    fig = figure(width=600, height=600)
-    fig.vbar(
-        x=[1, 2, 3, 4, 5],
-        width=0.5,
-        bottom=0,
-        top=[1.7, 2.2, 4.6, 3.9, 1.1],
-        color='navy'
-    )
-
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
-
-    # render template
-    script, div = components(fig)
-    html = render_template(
-        'index.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    return html
 
 
 @app.route('/')
@@ -49,7 +20,7 @@ def test():
     css_resources = INLINE.render_css()
 
     html = render_template(
-        template_name_or_list='embed.html',
+        template_name_or_list=template_html,
         script=script,
         div=div,
         js_resources=js_resources,
