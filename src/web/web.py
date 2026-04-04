04c2077e1a9623ff7eb0545de805414181e4cf77
Christian Braun <braun.christian.1994@gmail.com>
2026-04-04 10:42:24 +0200

    init

--- src/web/web.py
+++ src/web/web.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 from bokeh.embed import components
 from bokeh.plotting import figure
-from bokeh.resources import INLINE
+
 from bokeh.models import Plot
 
 
@@ -17,15 +17,23 @@ def prepare_compontents_graph(p: Plot) -> tuple[str, str]:
     return script_graph, div_graph
 
 
+def prepare_inline() -> tuple[str, str]:
+    from bokeh.resources import INLINE
+
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    return js_resources, css_resources
+
+
 @app.route('/')
-def test():
+def prepare_template_graph():
     import web_net as wb
     p = wb.get_netgraph()
 
     script_graph, div_graph = prepare_compontents_graph(p=p)
 
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
+    js_resources, css_resources = prepare_inline()
 
     html = render_template(
         template_name_or_list=template_html,
