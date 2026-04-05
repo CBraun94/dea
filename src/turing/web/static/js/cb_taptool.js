4fefaf1d04a014fccc1c9982a9c46924d860067d
Christian Braun <braun.christian.1994@gmail.com>
2026-04-05 22:18:31 +0200

    init

--- src/turing/web/static/js/cb_taptool.js
+++ src/turing/web/static/js/cb_taptool.js
@@ -2,9 +2,12 @@ const div_dst = '#ttt'
 
 var index = cb_data.source.selected.indices[0];
 var text = '';
+var dict = {};
 
 for (const property in cb_data.source.data) {
   text += `${property}: ${cb_data.source.data[property][index]}`;
+  dict[property] = cb_data.source.data[property][index]
 }
 
 $(div_dst).text(text);
+$(div_dst).text(JSON.stringify(dict));
