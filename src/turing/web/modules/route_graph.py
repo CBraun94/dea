fe71b4cb1bab08350fff794e85949da68c49fef6
Christian Braun <braun.christian.1994@gmail.com>
2026-04-05 13:13:18 +0200

    init

--- src/turing/web/modules/route_graph.py
+++ src/turing/web/modules/route_graph.py
@@ -23,51 +23,6 @@ import reader as r
 bp_p_graph = Blueprint('bp_p_graph', __name__)
 
 
-def prepare_compontents_graph(p: Plot) -> tuple[str, str]:
-    from bokeh.embed import components
-    from bokeh import themes
-    from bokeh.plotting import curdoc
-
-    curdoc().theme = 'carbon'
-    curdoc().add_root(p)
-
-    script_graph, div_graph = components(p, theme='carbon')
-
-    return script_graph, div_graph
-
-
-def prepare_inline() -> tuple[str, str]:
-    from bokeh.resources import INLINE
-
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
-
-    return js_resources, css_resources
-
-
-@bp_p_graph.route('/graph2')
-def prepare_template_graph():
-    from flask import render_template
-    import turing.web.modules.route_graph_util as wb
-    G = wb.get_net_data()
-    G = r.graph.graph_to_nx(r.mermaid_td.read_mermaid_flowchart(r.mermaid_td.mermaid.splitlines()))
-    p = wb.get_netgraph(G=G, plot_title='state diagram')
-
-    script_graph, div_graph = prepare_compontents_graph(p=p)
-
-    js_resources, css_resources = prepare_inline()
-
-    html = render_template(
-        template_name_or_list='__base_graph.html',
-        script=script_graph,
-        div=div_graph,
-        js_resources=js_resources,
-        css_resources=css_resources
-    )
-
-    return html
-
-
 def bkapp(doc):
     import web.modules.route_graph_util as wb
     doc.theme = 'carbon'
