a038dd11049c8de26c7e788ca8b271cf1585269b
Christian Braun <braun.christian.1994@gmail.com>
2026-04-06 13:06:35 +0200

    init

---
+++ src/turing/dataframe.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from uuid import UUID, uuid4
+import json
+
+
+class DataFrame(object):
+    def __init__(self):
+        self.uuid: UUID = uuid4()
+        self.df: dict[str, pd.DataFrame] = {}
+
+    def to_dict(self) -> dict:
+        _d = {}
+        _d['uuid'] = str(self.uuid)
+        _d['df'] = {}
+
+        for key in self.df:
+            _d['df'][key] = self.df[key].to_dict()
