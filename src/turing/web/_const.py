558c904cdac40ea08617e6e7fe8e747971360ec5
Christian Braun <braun.christian.1994@gmail.com>
2026-04-06 11:13:25 +0200

    init

---
+++ src/turing/web/_const.py
@@ -0,0 +1,5 @@
+PORT_FLASK: int = 8000
+
+ROUTE_APP_ROOT = '/'
+
+TEMPLATE_APP_ROUTE = r'__ide_gui.html'
--- src/turing/web/app.py
+++ src/turing/web/app.py
@@ -1,8 +1,9 @@
 import modules as m
 from flask import Flask, render_template
+import _const as _c
 
 
-template_html = r'__ide_gui.html'
+template_html = _c.TEMPLATE_APP_ROUTE
 
 app = Flask(__name__)
 app.register_blueprint(m.route_about.bp_p_about)
@@ -11,7 +12,7 @@ app.register_blueprint(m.route_graph.bp_p_graph)
 app.register_blueprint(m.route_menubar.bp_p_menubar)
 
 
-@app.route('/')
+@app.route(_c.ROUTE_APP_ROOT)
 def prepare_template_ide():
     script_graph = m.route_graph.get_graph_script()
     script_table = m.route_graph.get_table_script()
@@ -26,7 +27,7 @@ def prepare_template_ide():
 
 def run():
     t = m.route_graph.init()
-    app.run(port=8000)
+    app.run(port=_c.PORT_FLASK)
 
 
 if __name__ == '__main__':
