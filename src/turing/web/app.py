bc81370ff32a8ffb50f8d11c8572482143e5644f
Christian Braun <braun.christian.1994@gmail.com>
2026-04-07 20:11:04 +0200

    init

--- src/turing/web/app.py
+++ src/turing/web/app.py
@@ -12,16 +12,37 @@ app.register_blueprint(m.route_graph.bp_p_graph)
 app.register_blueprint(m.route_menubar.bp_p_menubar)
 
 
+def tree_find(e, t):
+    if e in t:
+        return t
+    for v in t.values():
+        r = tree_find(e, v)
+        if r:
+            return r
+    return None
+
+
+def template_treeview():
+    dict_ = {'A':['B', 'C'], 'B':['D','E'], 'C':['F', 'G', 'H'], 'E':['I', 'J']}
+    tree = {}
+    for k,v in dict_.items():
+        n = tree_find(k, tree)
+        (tree if not n else n)[k] = {e:{} for e in v}
+    return tree
+
+
 @app.route(_c.ROUTE_APP_ROOT)
 def prepare_template_ide():
     data = r"{'A': {'A': ['A'], 'B': ['A', 'B'], 'C': ['A', 'B', 'C'], 'D': ['A', 'B', 'C', 'D'], 'E': ['A', 'B', 'C', 'E'], 'F': ['A', 'B', 'C', 'F']}, 'B': {'B': ['B'], 'C': ['B', 'C'], 'D': ['B', 'C', 'D'], 'E': ['B', 'C', 'E'], 'F': ['B', 'C', 'F']}, 'C': {'C': ['C'], 'D': ['C', 'D'], 'E': ['C', 'E'], 'F': ['C', 'F']}, 'D': {'D': ['D']}, 'E': {'E': ['E']}, 'F': {'F': ['F']}}"
     script_graph = m.route_graph.get_graph_script()
     script_table = m.route_graph.get_table_script()
+    treeview = template_treeview()
     html = render_template(
         template_name_or_list=template_html,
         script_graph=script_graph,
         script_table=script_table,
-        data=data
+        data=data,
+        tree=treeview
     )
 
     return html
