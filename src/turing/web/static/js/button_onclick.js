82c0686c178383188055728eefccb294b3d04007
Christian Braun <braun.christian.1994@gmail.com>
2026-04-08 21:32:16 +0200

    init

--- src/turing/web/static/js/button_onclick.js
+++ src/turing/web/static/js/button_onclick.js
@@ -1,3 +1,8 @@
-function buttonOnClick(evt, IdSender, T_Event) {
-    alert('test')
+function buttonOnClick(test, IdSender, T_Event) {
+    console.log(test);
+    console.log(T_Event);
+    console.log(IdSender);
+
+    e = document.getElementById(this.id);
+    e.textContent = "click";
 }
