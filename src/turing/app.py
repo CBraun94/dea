56354f90715adeeba27d307d0fb3ad547c7783f1
Christian Braun <braun.christian.1994@gmail.com>
2026-04-08 18:48:08 +0200

    init

--- src/turing/app.py
+++ src/turing/app.py
@@ -1,16 +1,24 @@
 from turing.dataframe import DataFrame
+from typing import Dict, Optional
 
 
 class Application(object):
     def __init__(self):
-        self.df: dict[str, DataFrame] = {}
-        self.data: dict[str] = {}
+        self.df: Dict[str, DataFrame] = {}
+        self.data: Dict[str] = {}
+        self.env: Dict[str, Optional[str]] = {}
 
         _df: DataFrame = DataFrame()
         self.df[str(_df.uuid)] = _df
 
+        self.__init_env()
+
     def get_df_first(self) -> DataFrame:
         return self.df[next(iter(self.df))]
+
+    def __init_env(self):
+        import dotenv
+        self.env = dotenv.dotenv_values()
 
 
 app: Application = Application()
