aa25b257c3fd240dc2e94e42e97917e65f08bac1
Christian Braun <braun.christian.1994@gmail.com>
2026-04-09 10:55:24 +0200

    init

--- src/turing/web/app.py
+++ src/turing/web/app.py
@@ -37,23 +37,33 @@ def template_treeview(dict_: dict):
     return tree
 
 
+def test():
+    import os
+    fn = os.getenv('TURING_PATH_OUTPUT')+'edges.html'
+    r: str = ''
+    if os.path.exists(fn):
+        with open(file=fn, mode='r') as f:
+            r = f.read()
+
+    return r
+
+
 @app.route(_c.ROUTE_APP_ROOT)
 def prepare_template_ide():
     from turing.app import app as t_app
 
     dict_ = {'files': t_app.data['files'].copy()}
 
-    data = {'test':{'A': {'A': ['A'], 'B': ['A', 'B'], 'C': ['A', 'B', 'C'], 'D': ['A', 'B', 'C', 'D'], 'E': ['A', 'B', 'C', 'E'], 'F': ['A', 'B', 'C', 'F']}, 'B': {'B': ['B'], 'C': ['B', 'C'], 'D': ['B', 'C', 'D'], 'E': ['B', 'C', 'E'], 'F': ['B', 'C', 'F']}, 'C': {'C': ['C'], 'D': ['C', 'D'], 'E': ['C', 'E'], 'F': ['C', 'F']}, 'D': {'D': ['D']}, 'E': {'E': ['E']}, 'F': {'F': ['F']}}}
+    data = test()
     script_graph = m.route_graph.get_graph_script()
     script_table = m.route_graph.get_table_script()
     treeview = template_treeview(dict_)
-    tv_data = template_treeview(data)
 
     context = {}
     context['template_name_or_list'] = template_html
     context['script_graph'] = script_graph
     context['script_table'] = script_table
-    context['data'] = tv_data
+    context['data'] = data
     context['tree'] = treeview
 
     html = render_template(**context)
